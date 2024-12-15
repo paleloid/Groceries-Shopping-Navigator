0b4a2d91c3f46db7c3a99ccfab65851206b92b95
Mher <barsmherg@gmail.com>
2024-12-16 00:28:47 +0400

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
 
 public class Client
@@ -6,17 +6,26 @@ public class Client
     public String name;
     public List<String> shoppingList = new ArrayList<String>();
     public String shoppingItems;
-    public Client(String name, List<String> shoppingList)
+
+    public Client (String shoppingItems)
+    {
+        this.shoppingItems = shoppingItems;
+    }
+    public Client (String name, List<String> shoppingList)
     {
         this.name = name;
         this.shoppingList = shoppingList;
-        shoppingItems();
+        shoppingItemsConverter();
     }
-    public void shoppingItems()
+    public void shoppingItemsConverter()
     {
         String items = "";
         for (String item : shoppingList)
             items = items.concat(item.toLowerCase() + " ");
         this.shoppingItems = items;
+    }
+    public String getShoppingItems()
+    {
+        return shoppingItems;
     }
 }
